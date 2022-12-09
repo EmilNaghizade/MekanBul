c3ef1563c984f28b80d14b88992f4710fe2c9d6d
Emil <emil.nagizade233@gmail.com>
2022-12-09 13:17:38 +0300

    aaaaa

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 var apiSecenekleri = {
   // sunucu: "http://localhost:3000",
-  sunucu: "https://MekanBul.xalturwik.repl.co",
+  sunucu: "https://mekanbul.xalturwik.repl.co/",
   apiYolu: "/api/mekanlar/",
 };
 var mesafeyiFormatla = function (mesafe) {
